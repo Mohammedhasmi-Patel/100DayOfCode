f1728a4acc3f0f83abb8fc1d01c29d8485dcafa2
Mohammedhasmi-Patel <patelmohammedhasmi@gmail.com>
2024-07-11 11:57:00 +0530

    Day5 done successfully....

---
+++ Day5.java
@@ -0,0 +1,60 @@
+public class Day5 {
+  // check the given string is palindrome or not 
+
+  static boolean isPalindrome(String str,int start,int end){
+    // base condition 
+    if(start>=end){
+      return true;
+    }
+    if(str.charAt(start)!=str.charAt(end)){
+      return false;
+    }
+    return isPalindrome(str, start+1, end-1);
+  }
+
+  // count total No of vowels in string 
+
+  static int countVowels(String str,int index){
+    // try to solve from last index so that we dont need to pass extra argument
+    if(index==-1){
+      return 0;
+    }
+    // we will found first vowel so just adding by one 
+    if(str.charAt(index)=='a' || str.charAt(index)=='e' || str.charAt(index)=='i' || str.charAt(index)=='o' || str.charAt(index)=='u' ){
+      return 1 + countVowels(str, index-1);
+    }
+    return countVowels(str, index-1);
+  }
+
+  
+
+  // reverse the string using recursion 
+
+  static void reverse(char [] arr,int start,int end){
+      if(start>=end){
+        return;
+      }
+      // swap first and last character
+      char temp = arr[start];
+      arr[start] = arr[end];
+      arr[end] =temp;
+
+      // now call recursive 
+      reverse(arr, start+1, end-1);   
+  }
+  // convert lowercase to uppercase using recursion
+
+  static void lowerToUpper(char [] str,int index){
+    if(index==-1){
+      return;
+    }
+    str[index] = (char) ((char)str[index]-'a'+'A');
+    lowerToUpper(str, index-1);
+  }
+  public static void main(String[] args) {
+    char [] arr = {'h','a','s','m','i'};
+    lowerToUpper(arr, 4);
+    System.out.println(arr);
+  }
+  
+}
